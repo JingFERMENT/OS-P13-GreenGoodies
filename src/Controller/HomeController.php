f220184277a9b4f5aef6390926e6bbe4342225a7
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-07-11 18:36:22 +0200

    feat: display products dynamically and add individual product detail page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+        $products = $productRepository->findAll();
+
+         return $this->render('home/home.html.twig', [
+            'products' => $products,
         ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product/{id}', name: 'app_product', requirements: ['id' => '\d+'])]
+    public function show(ProductRepository $productRepository, int $id): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+
+        $product = $productRepository->find($id);
+        
+        return $this->render('product/product.html.twig', [
+             'product' => $product,
         ]);
     }
 }
