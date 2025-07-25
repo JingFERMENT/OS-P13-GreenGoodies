be4486f29cc8d75ec67b1384300a7d78b6468163
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-07-25 18:35:22 +0200

    feat: feat: complete cart page and validation flow
    - Implement cart page (Twig HTML & CSS)
    - Create 4 routes in CartController: addCart, showCart, clearCart, validateCart
    - Refactor and update CartService: add getCartDetails() and getTotalPrice(), update addCart()
    - Add flash messages for cart confirmation and link in base.html.twig
    - Control access (authenticated user or not) and adapt wording for validate button on product page
    - Save cart data to database (Order and OrderLine)

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\OrderLine;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\CartService;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,39 +21,67 @@ final class CartController extends AbstractController
     public function addToCart(Product $product, Request $request, CartService $cartService): Response
     {
         $quantity = $request->request->get('quantity', 1);
-      
-        if($quantity > 0) {
-            $cartService->add($product, $quantity);
-        } 
-
+        $cartService->add($product, $quantity);
+        
         return $this->redirectToRoute('app_cart');
     }
 
     #[Route('/cart', name: 'app_cart')]
     public function showCart(CartService $cartService, ProductRepository $productRepository): Response
     {
-        $cart = $cartService->getCart();
-    
-        $cartDetails = [];
-        $totalPrice = 0;
-        foreach ($cart as $productID => $quantity) {
-            
-            $product = $productRepository->find($productID);
-            $lineTotal = $product->getPrice() * $quantity;
-            $cartDetails[] = [
-                'product' => $product,
-                'quantity' => $quantity,
-                'totalOfOneProduct' => $lineTotal,
-            ];
-
-            $totalPrice += $lineTotal;
-        }
+       $cartDetails = $cartService->getCartDetails($productRepository);
+       $totalPrice = $cartService->getTotalPrice($productRepository);
         
-
         return $this->render('cart/cart.html.twig', [
             'cartDetails' => $cartDetails,
             'totalPrice' => $totalPrice,
         ]);
     }
 
+    #[Route('/clear-cart', name: 'app_clear_cart')]
+    public function clearCart(CartService $cartService): Response
+    {
+        $cartService->clear();
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/validate-cart', name: 'app_validate_cart')]
+    public function validateCart(Request $request, 
+    CartService
+    $cartService, 
+    ProductRepository $productrepository, 
+    Security $security, EntityManagerInterface $entityManager): Response{
+
+        $user = $security->getUser();
+   
+        $cartDetails = $cartService->getCartDetails($productrepository);
+
+        $totalPrice = $cartService ->getTotalPrice($productrepository);
+       
+        // create a new order 
+        $order = new Order();
+        $order->setUser($user);
+        $order->setPaidPrice($totalPrice);
+        $order->setCreatedAt(new \DateTimeImmutable());
+
+         $entityManager->persist($order);
+
+        // create a new orderline
+        forEach($cartDetails as $oneCart) {
+             $orderline = new OrderLine();
+             $orderline->setOrderGoodies($order);
+             $orderline->setProduct($oneCart['product']);
+             $orderline->setQuantity($oneCart['quantity']);
+
+             $entityManager->persist($orderline);
+        }
+
+        // save all to DB
+        $entityManager->flush();
+        
+        $cartService->clear();
+
+        $this->addFlash('success', 'Votre commande a été validée avec succès !');
+        return $this->redirectToRoute('app_home');
+    }
 }
--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -3,38 +3,70 @@
 namespace App\Service;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class CartService {
+class CartService
+{
 
     private $session;
 
-    public function __construct(RequestStack $requestStack) {
+    public function __construct(RequestStack $requestStack)
+    {
         $this->session = $requestStack->getSession();
     }
-    
+
     public function add(Product $product, int $quantity): void
     {
         // Logic to add the product to the cart with the specified quantity
         $cart = $this->session->get('cart', []);
-        
-        $productId = $product->getId();
 
-        if (isset($cart[$productId])) {
-            $cart[$productId] += $quantity; // Increment quantity if product already in cart
-        } else {
-            $cart[$productId] = $quantity; // Add new product to cart
-        }
+
+        $productId = $product->getId();
+        $this->remove($product);
+
+        $cart[$productId] = $quantity; // Add new product to cart
 
         $this->session->set('cart', $cart);
-
     }
 
     public function getCart(): array
     {
         // Logic to retrieve the current cart contents
         return $this->session->get('cart', []);
+    }
+
+    public function getCartDetails(ProductRepository $productRepository): array
+    {
+        $cart = $this->getCart();
+        $cartDetails = [];
+
+        foreach ($cart as $productID => $quantity) {
+            $product = $productRepository->find($productID);
+            if (!$product) {
+                continue; // Ignorer les produits supprimés de la BDD
+            }
+            $totalPriceOfOneProduct = $product->getPrice() * $quantity;
+            
+            $cartDetails[] = [
+                'product' => $product,
+                'quantity' => $quantity,
+                'totalPriceOfOneProduct' => $totalPriceOfOneProduct,
+            ];
+        }
+
+        return $cartDetails;
+    }
+
+    public function getTotalPrice(ProductRepository $productRepository){
+        $totalPrice = 0;
+
+        foreach ($this->getCartDetails($productRepository) as $oneCart) {
+            $totalPrice +=$oneCart['totalPriceOfOneProduct'];    
+        }
+
+        return $totalPrice;
     }
 
     public function remove(Product $product): void
