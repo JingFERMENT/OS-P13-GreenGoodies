0531a785dd5bddc1319b524f84098d5777e59370
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-07-30 22:39:32 +0200

    feat: activate API access with button and UserChecker validation
    - Add route to toggle API access (activation/deactivation)
    - Update Twig template with API activation button
    - Implement UserChecker to restrict API login if access not activated
    - Configure security.yaml to support API restrictions

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ApiController extends AbstractController
 {
@@ -20,7 +22,28 @@ final class ApiController extends AbstractController
         $jsonProducts = $serializer->serialize($products, 'json');
 
         return new JsonResponse(
-            $jsonProducts, Response::HTTP_OK, [], true
+            $jsonProducts,
+            Response::HTTP_OK,
+            [],
+            true
         );
+    }
+
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/activateApi', name: 'app_activate_api', methods: ['POST'])]
+    public function activateApi(Security $security, 
+    EntityManagerInterface $entityManager): Response
+    {
+        $user = $security->getUser();
+
+        if ($user->isActivatedAPI()) {
+            $user->setIsActivatedAPI(false);
+        } else {
+            $user->setIsActivatedAPI(true);
+        };
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_my_account');
     }
 }
