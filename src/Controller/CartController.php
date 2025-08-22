9e2b852cd60508825c150d52a466097439f47d4c
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-08-22 15:12:38 +0200

    feat(cart): add validateCartForm fix(auth): auto-login after registration fix(products): make products page accessible to all fix(auth): customize login error messages in French (security.fr.yaml)

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\OrderLine;
+use App\Form\ValidateCartFormType;
 use App\Repository\ProductRepository;
 use App\Service\CartService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,18 +19,24 @@ final class CartController extends AbstractController
 {
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/cart', name: 'app_cart')]
-    public function showCart(Security $security, CartService $cartService, ProductRepository $productRepository): Response
-    {
+    public function showCart(
+        Security $security,
+        CartService $cartService,
+        ProductRepository $productRepository,
+    ): Response {
 
         $user = $security->getUser();
-        
+
         $cartDetails = $cartService->getCartDetails($productRepository);
-       
+
         $totalPrice = $cartService->getTotalPrice($productRepository);
+
+        $form = $this->createForm(ValidateCartFormType::class);
 
         return $this->render('cart/cart.html.twig', [
             'cartDetails' => $cartDetails,
             'totalPrice' => $totalPrice,
+            'validateCartForm' => $form->createView(),
         ]);
     }
 
@@ -43,11 +51,11 @@ final class CartController extends AbstractController
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/validate-cart', name: 'app_validate_cart')]
     public function validateCart(
-        CartService
-        $cartService,
+        CartService $cartService,
         ProductRepository $productrepository,
         Security $security,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Request $request,
     ): Response {
 
         $user = $security->getUser();
@@ -62,24 +70,36 @@ final class CartController extends AbstractController
         $order->setPaidPrice($totalPrice);
         $order->setCreatedAt(new \DateTimeImmutable());
 
-        $entityManager->persist($order);
+        $form = $this->createForm(ValidateCartFormType::class);
+        $form->handleRequest($request);
 
-        // create a new orderline
-        foreach ($cartDetails as $oneCart) {
-            $orderline = new OrderLine();
-            $orderline->setOrderGoodies($order);
-            $orderline->setProduct($oneCart['product']);
-            $orderline->setQuantity($oneCart['quantity']);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($order);
 
-            $entityManager->persist($orderline);
+            // create a new orderline
+            foreach ($cartDetails as $oneCart) {
+                $orderline = new OrderLine();
+                $orderline->setOrderGoodies($order);
+                $orderline->setProduct($oneCart['product']);
+                $orderline->setQuantity($oneCart['quantity']);
+
+                $entityManager->persist($orderline);
+            }
+
+            // save all to DB
+            $entityManager->flush();
+
+            $cartService->clear();
+
+            $this->addFlash('success', 'Votre commande a été validée avec succès !');
+
+            return $this->redirectToRoute('app_home');
         }
 
-        // save all to DB
-        $entityManager->flush();
-
-        $cartService->clear();
-
-        $this->addFlash('success', 'Votre commande a été validée avec succès !');
-        return $this->redirectToRoute('app_home');
+        return $this->render('cart/cart.html.twig', [
+        'cartDetails' => $cartDetails,
+        'totalPrice' => $totalPrice,
+        'validateCartForm' => $form->createView(),
+    ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,16 +6,20 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request, 
+        UserPasswordHasherInterface $userPasswordHasher, 
+        EntityManagerInterface $entityManager,
+        Security $security): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -31,8 +35,10 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            // Auto-login after registration
+            $security->login($user);
 
+            $this->addFlash('success', 'Inscription réussie ! Vous êtes maintenant connecté(e).');
             return $this->redirectToRoute('app_home');
         }
 
