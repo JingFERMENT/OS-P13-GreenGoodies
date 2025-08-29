02b637db4366b59f1f0ea9052b4f7cbed9de78f5
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-08-29 11:50:57 +0200

    Fix: add a FormEvent to prevent Symfony from resetting quantity to 0 when the user has previously selected a value

--- src/Form/CartQuantityFormType.php
+++ src/Form/CartQuantityFormType.php
@@ -6,6 +6,7 @@ use App\Entity\OrderLine;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CartQuantityFormType extends AbstractType
@@ -13,12 +14,25 @@ class CartQuantityFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('quantity', ChoiceType::class, [ 
+            ->add('quantity', ChoiceType::class, [
                 'choices' => array_combine(range(0, 5), range(0, 5)), // symfony expects label => value
-                'label' => 'Quantité', 
+                'label' => 'Quantité',
                 'required' => true,
                 'attr' => ['class' => 'quantity-dropdown'],
             ]);
+
+        // This solves the issue by using "data => 0" to reset the dropdown 
+        //  to 0, even if the user had previously selected another value
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function ($event) {
+            $orderLine = $event->getData();
+            if (!$orderLine) {
+                return;
+            }
+
+            if ($orderLine->getQuantity() === null) {
+                $orderLine->setQuantity(0);
+            };
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
